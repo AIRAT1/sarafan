e952a1afa11380cd3eb7d00feae67a3c9a557f93
Ayrat <ayrat1@mail.ru>
2020-10-19 00:12:30 +0300

    init project lesson 19 complete

---
+++ src/test/java/de/sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
