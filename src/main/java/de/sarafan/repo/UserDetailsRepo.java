2a28b57f2b54a1945e25a40e3fa9c7cdd82dc47d
Ayrat <ayrat1@mail.ru>
2020-10-19 20:35:44 +0300

    lesson 24 complete add Spring Security and oAuth2 authorization

---
+++ src/main/java/de/sarafan/repo/UserDetailsRepo.java
@@ -0,0 +1,7 @@
+package de.sarafan.repo;
+
+import de.sarafan.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDetailsRepo extends JpaRepository<User, String> {
+}
